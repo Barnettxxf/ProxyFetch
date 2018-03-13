4119825bda6380117a02ec49ff1e9db6c267b2ae
Barnettxxf <15102096586@163.com>
2018-03-13 18:32:25 +0800

    Initial Commit

---
+++ config_MySQL.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+'''
+author: Barnett
+'''
+
+
+LOCALCONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': 'xxf99311',
+    'db': 'jobdata',
+    'charset': 'utf8'
+}
+
+
+
