1c9065f2988b2af5872d62bc7a19aecf2bc9b4ff
Barnettxxf <15102096586@163.com>
2018-03-17 01:32:08 +0800

    use gevent

--- run_fetch.py
+++ run_fetch.py
@@ -5,19 +5,30 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fectch_goubanjia import Proxygoubanjia
 from fectch_mimvp import Proxymimvp
 from fectch_xici import ProxyxiciIp
+import gevent
+from gevent import monkey
+monkey.patch_all()
 
 li = [Proxygoubanjia, Proxymimvp, ProxyxiciIp]
 
 p_list = []
 
-for c in li:
-    p_list.append(c())
 
-for p in p_list:
+def run(p):
     p.update()
     if hasattr(p, 'save_image'):
         p.save_image()
-
-for p in p_list:
     p.close()
+
+
+for c in li:
+    p_list.append(c())
+
+t_list = []
+for p in p_list:
+    t = gevent.spawn(run, p)
+    t_list.append(t)
+
+for t in t_list:
+    t.join()
 
