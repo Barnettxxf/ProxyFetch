6541308d8fd7ddbacb3d034a5ff17802d56ae9c9
Barnettxxf <15102096586@163.com>
2018-03-18 13:08:56 +0800

    modify filter_ip

--- filter_ip.py
+++ filter_ip.py
@@ -1,99 +1,126 @@
 # -*- coding:utf-8 -*-
-import pandas as pd
+
+import pymysql
 import time
-from ProxyFecth.config_MySQL import LOCALCONFIG
-from requests.adapters import HTTPAdapter
+from config_MySQL import MyAliyunServer as CONFIG
 import requests
-from sqlalchemy.engine import create_engine
-import logging
-import threading
+from threading import Thread
+from queue import Queue
+from urllib3 import disable_warnings; disable_warnings()
 
+result = Queue()
 
-password = LOCALCONFIG['password']
-count = 0
-engine = create_engine(f'mysql+pymysql://root:{password}@localhost:3306/jobdata')
-request_retry = HTTPAdapter(max_retries=3)
+class GetIP(object):
 
+    test_url = 'https://www.baidu.com'
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/64.0.3282.167 Chrome/64.0.3282.167 Safari/537.36'}
+    tables = ['proxymimvp', 'proxyxici', 'proxygoubanjia']
 
-class FilterIp(object):
+    def __init__(self, timeout=10):
+        self.conn = pymysql.connect(**CONFIG)
+        self.cursor = self.conn.cursor()
+        self.timeout = timeout
 
-    def spawn_run(self):
-        data_list = self._get_data()
-        tasks = []
-        for i in range(len(data_list)):
-            task = threading.Thread(target=self.run, args=(data_list[i],))
-            tasks.append(task)
-        for task in tasks:
-            task.start()
-        for task in tasks:
-            task.join()
+    @property
+    def ip_list(self):
+        ip_list = []
+        while len(ip_list) < 7:
+            result = self.get_ip()
+            ip_list.extend(result)
+        return ip_list
 
-    def run(self, data):
-        for i in range(data.shape[0]):
-            ip, port = data.loc[i, ['ip', 'port']].values[0], data.loc[i, ['ip', 'port']].values[1]
-            self._filter(ip, port)
+    def get_ip(self):
+        tables = self.tables
+        datas = []
+        for table_name in tables:
+            data = self.get_data(table_name)
+            data = self.filter_data(table_name, data)
+            datas.append(data)
+        result = []
+        for i in range(len(datas)):
+            result.extend(datas[i])
+        return result
 
-    def _filter(self, ip, port):
-        global count
-        count += 1
-        if self._checkproxy(ip, port):
-            with open('filter_ip.csv', 'w') as f:
-                f.write(ip + ':' + port + '\n')
-            logging.info(f'{count}-Succeeded!{ip}:{port}')
-        else:
-            logging.error(f'{count}-Failed!{ip}:{port}')
-
-    def _checkproxy(self, ip, port):
-        test_url = 'https://www.baidu.com'
-        # proxy_url = f'123.53.86.158:24866'
-        proxy_url = ip + ':' + port
-        proxy_dict = {'https': proxy_url}
+    def check(self, ip, port):
+        test_url = self.test_url
+        proxies = {'https': ip+':'+port}
+        print('Testing: ', ip+':'+port)
         try:
-            response = requests.get(test_url, headers=self.headers, proxies=proxy_dict, timeout=5)
+            requests.get(test_url, headers=self.headers, proxies=proxies, timeout=self.timeout, verify=False)
         except Exception as e:
-            logging.error(e)
             return False
+        print('Suceeded: ', ip+':'+port)
         return True
 
-    def _get_data(self):
-        data = pd.read_sql('proxyhttps', engine)
-        size = 10
-        num = data.shape[0] // size + 1
-        print(num)
-        data_list = []
-        for i in range(1, size):
-            new = data.loc[((i - 1) * num):(i * num), :]
-            new = new.reset_index(drop=True)
-            data_list.append(new)
-        return data_list
+    def filter_data(self, table_name, data):
+        new_datas = []
+        tasks = []
+        for each in data:
+            self.update_db(table_name, each[0])
+            t = Thread(target=self._filter_date, args=(table_name, each, ))
+            tasks.append(t)
+        for t in tasks:
+            t.start()
+        for t in tasks:
+            t.join()
+        while not result.empty():
+            new_datas.append(result.get())
+        return new_datas
 
-    def _test(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/64.0.3282.167 Chrome/64.0.3282.167 Safari/537.36',
-        }
-        response = requests.get('https://www.huxiu.com/', headers=headers, proxies={'http': '127.0.0.1:8888'})
-        response_1 = requests.get('http://www.meizi.com/', proxies={'http': '127.0.0.1:8888'})
-        print(response)
-        print(response_1)
-        print(response.text)
-        print(response_1.text)
+    def _filter_date(self, table_name, each):
+        if self.check(each[0], each[1]):
+            result.put(each)
+            self.set_enable_flag(table_name, each[0])
+
+    def get_data(self, table_name):
+        select_sql = """
+            SELECT ip, port, proxy_type FROM %s WHERE flag=0 ORDER BY 
+            date_time desc limit 10;
+            """ % table_name
+        self.execute(select_sql)
+        data = self.cursor.fetchall()
+        new_data = []
+        for each in data:
+            if ('HTTPS' not in each[2]) and ('https' not in each[2]):
+                continue
+            else:
+                new_data.append(each)
+        return new_data
+
+    def update_db(self, table_name, ip):
+        update_sql = """
+                    UPDATE {} SET flag=1 WHERE ip='{}'
+                    """.format(table_name, ip)
+        self.execute(update_sql)
+
+    def set_enable_flag(self, table_name, ip):
+        update_sql = """
+                    UPDATE {} SET flag=2 WHERE ip='{}'
+                    """.format(table_name, ip)
+        self.execute(update_sql)
+
+    def execute(self, sql):
+        self.cursor.execute(sql)
+        self.conn.commit()
+
+    def close(self):
+        # self.recover()
+        self.cursor.close()
+        self.conn.close()
+
+    def recover(self):
+        for each in self.tables:
+            recover_sql = """
+            update %s set flag=0 where flag!=0;     
+        """
+            self.execute(recover_sql % each)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                        datefmt='%a, %d %b %Y %H:%M:%S',
-                        filename='filter.log',
-                        filemode='w')
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
+    t = GetIP(4)
+    start_time = time.time()
+    ds = t.ip_list
+    print('ruesult: ', ds)
+    print('Total cost {} seconds'.format(time.time() - start_time))
+    t.close()
 
-    start = time.time()
-    logging.info('########################Filtering Now########################')
-    t = FilterIp()
-    t.spawn_run()
-    logging.info('########################Finished Yet########################')
-    print('******************Cost {} seconds******************'.format(time.time() - start))
